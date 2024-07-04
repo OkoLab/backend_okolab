64443f047d48938516c80f919a41492e1a6c1e2e
root <root@LAPTOP-SHHPJ8L2>
2024-07-04 12:00:58 +0300

    0.0.1

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('user', function (Request $request) {
-        return [
-            'user' => $request->user(),
-            'token' => $request->bearerToken()
-        ];
-    });
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-    Route::post('user/logout', [UserController::class, 'logout']);
-});
 
-Route::post('user/login', [UserController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
