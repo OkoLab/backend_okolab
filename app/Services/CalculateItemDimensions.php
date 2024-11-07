cf915e50767b718e08392917906be794ea99e459
root <root@LAPTOP-SHHPJ8L2>
2024-11-07 13:26:11 +0300

    create method getTotalVolume

--- app/Services/CalculateItemDimensions.php
+++ app/Services/CalculateItemDimensions.php
@@ -2,13 +2,23 @@
 
 namespace App\Services;
 
-use Dimensions;
+use App\Types\Dimensions;
+use App\Types\Product;
+use Exception;
 
-
-// TODO 05/11/2024 Создать интерфейс объекта с артикулами оборудования и количеством в посылке
-// Создать функцию которая принимает этот объект из контроллера и рассчитывает общий объем всего оборудования
+// TODO 07/11/2024 Создать функцию которая принимает объект с артикулом и количеством и рассчитывает общий объем всего оборудования
 class CalculateItemDimensions {
     private $default_box_size = new Dimensions(37, 21, 7);
 
+    public function getTotalVolume(array $products): int {
+        $result = 0;
+        foreach ($products as $product) {
+           if ($product instanceof Product) {
 
+           } else {
+                throw new Exception("Invalid object passed. Expected instance of MyClass.");
+           }                   
+        }
+        return $result;
+    } 
 }
