83d234177edc6778546e0f17c43269fa25059730
root <root@LAPTOP-SHHPJ8L2>
2024-11-14 12:09:33 +0300

    Dimensions сделал , ,  protected

--- app/Types/Dimensions.php
+++ app/Types/Dimensions.php
@@ -4,13 +4,13 @@ namespace App\Types;
 
 class Dimensions {
     protected int $width, $height, $length;
-    
+
     public function __construct(int $width, int $height, int $length) {
         $this->width = $width;
         $this->height = $height;
         $this->length = $length;
     }
-    
+
     public function getVolume(): int {
         return $this->width * $this->length * $this->height;
     }
