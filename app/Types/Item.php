f0dff821f165e1eda576fcab565e8228f17c76a9
root <root@LAPTOP-SHHPJ8L2>
2024-11-14 12:08:21 +0300

    Создал класс Item, который хранит длину, ширину, высоту, вес отравления

---
+++ app/Types/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Types\Dimensions;
+
+class Item extends Dimensions {
+    private int $weight;
+
+    public function __construct(int $width, int $height, int $length, int $weight) {
+        parent::__construct($width, $height, $length);
+        $this->weight = $weight;
+    }
+
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+}
