80a7437683000ec680883fa58551b6416ae7fb81
root <root@LAPTOP-SHHPJ8L2>
2024-11-14 10:58:03 +0300

    app/Services/CalculateItemDimensions.php

--- app/Services/CalculateItemDimensions.php
+++ app/Services/CalculateItemDimensions.php
@@ -4,25 +4,36 @@ namespace App\Services;
 
 use App\Types\Dimensions;
 use App\Types\Product;
+use App\Models\DevicesBoxSize;
 use Exception;
 
 // TODO 07/11/2024 Создать функцию которая принимает объект с артикулом и количеством и рассчитывает общий объем всего оборудования
 class CalculateItemDimensions {
-    //private $default_box_size = new Dimensions(37, 21, 7);
+    private $default_box_size;
 
     public function __construct() {
-
+        $this->default_box_size = new Dimensions(37, 21, 7);
     }
 
-    public function getTotalVolume(array $products): int {
-        $result = 0;
-        // foreach ($products as $product) {
-        //    if ($product instanceof Product) {
+    public function getTotalVolume(array $selected_product_articles): int {
+        $sum_volume = 0;
+        $sum_weight = 0;
+        foreach ($selected_product_articles as $product_article) {
+            foreach ($product_article as $article => $amount) {
+                $item = DevicesBoxSize::where('article', $article)->first();
+                $volume = $item->length * $item->width * $item->height;
+                $sum_volume += $volume * $amount;
+                $sum_weight += $item->weight * $amount;
+            }
+        }
 
-        //    } else {
-        //         throw new Exception("Invalid object passed. Expected instance of MyClass.");
-        //    }
-        // }
+        $coefficient = ceil($sum_volume / $this->default_box_size->getVolume());
+        $number_of_items = ceil($coefficient / 10);
+
+        $item_weight = ceil($sum_weight / $number_of_items) + 1;
+
+
+
         return $result;
     }
 }
