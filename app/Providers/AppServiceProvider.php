ceca900410c6fef5b5d32db2fca51403ef09d399
root <root@LAPTOP-SHHPJ8L2>
2024-11-16 17:10:12 +0300

    Сделан CdekApiService singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\CdekApiService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(CdekApiService::class, function ($app) {
+            return new CdekApiService();
+        });
     }
 
     /**
